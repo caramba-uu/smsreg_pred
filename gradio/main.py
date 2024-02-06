00d69261bc18ee6308405731cd8d2ff30deb4abb
Akshai <35807615+akshaips@users.noreply.github.com>
2024-02-06 09:40:20 +0100

    scilife serve version

--- gradio/main.py
+++ gradio/main.py
@@ -176,4 +176,4 @@ with gr.Blocks() as demo:
                                     )
 
 if __name__ == "__main__":
-   demo.launch(share=True,server_name="0.0.0.0",server_port=8000)
+   demo.launch(share=False,server_name="0.0.0.0",server_port=8080)
